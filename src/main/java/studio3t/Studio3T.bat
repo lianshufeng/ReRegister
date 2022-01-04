d022d6cfe8dd2ed63fdd538a2a1908896dc45db4
lianshufeng <251708339@qq.com>
2022-01-04 11:10:14 +0800

    批处理

---
+++ src/main/java/studio3t/Studio3T.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:: remove reg
+reg delete "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\3t\mongochef\enterprise" /f
+
+:: remove diskfile
+rd /s /q %USERPROFILE%\\AppData\\Local\\ftuwWNWoJl-STeZhVGHKkQ--
+rd /s /q %USERPROFILE%\\AppData\\Local\\t3
+rd /s /q %USERPROFILE%\\AppData\\Local\\Temp\\ftuwWNWoJl-STeZhVGHKkQ--
+rd /s /q %USERPROFILE%\\AppData\\Local\\Temp\\t3
+rd /s /q %PUBLIC%\\t3
+rd /s /q %USERPROFILE%\\.cache
+rd /s /q %USERPROFILE%\\.3T\\studio-3t\\soduz3vqhnnja46uvu3szq--
+
+
+echo finish...
+echo -----------------------------------------
+pause
