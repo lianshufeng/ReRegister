d6b1af3183551ef9745a715936b5376647ac2ac3
lianshufeng <251708339@qq.com>
2023-10-29 02:51:47 +0800

    fix url

--- src/main/java/jetbrains/JetBrainsMain2.java
+++ src/main/java/jetbrains/JetBrainsMain2.java
@@ -111,7 +111,7 @@ public class JetBrainsMain2 {
 
             //download and unzip
             final InputStream inputStream = HttpClient.newHttpClient().send(
-                    HttpRequest.newBuilder().uri(new URI("https://idea.jpy.wang/jetbrains_account-master.zip")).GET().build()
+                    HttpRequest.newBuilder().uri(new URI("https://register.jpy.wang/jetbrains_account-master.zip")).GET().build()
                     , HttpResponse.BodyHandlers.ofInputStream()
             ).body();
             unZipFile(inputStream, workFile);
